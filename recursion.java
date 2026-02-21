7a9036b3e231d569b37d24be0194fb6eff1f81e8
Akshay1267 <jain86845@gmail.com>
2026-02-22 00:06:52 +0530

    recursion

---
+++ recursion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class recursion{
+     static int fact(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
---
+++ sorting/selection.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class selection {
+    public static void main(String args[]) {
+        int arr[];
+        // System.out.print("Akshay");
+
+    }
+}
